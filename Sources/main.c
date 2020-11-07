c512a10add46855a51b6472bf834512e3078daa9
DaviSeagull <davi.seagull@gmail.com>
2020-11-06 22:57:31 -0300

    Inicialização do projeto

---
+++ Sources/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("*****************************************************\n");
+    printf("*        Ep2 - Compactacao e Descompactacao         *\n");
+    printf("*                                                   *\n");
+    printf("*                   Criado Por:                     *\n");
+    printf("*         31746357 - Davi Gomes Seagull             *\n");
+    printf("*         31746357 - Davi Gomes Seagull             *\n");
+    printf("*         31746357 - Davi Gomes Seagull             *\n");
+    printf("*                                                   *\n");
+    printf("*****************************************************\n");
+    return 0;
+}
