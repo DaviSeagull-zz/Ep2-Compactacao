1f86183d24055f49e15fb6f83bee8c56a76dbbbf
DaviSeagull <davi.seagull@gmail.com>
2020-11-06 23:03:14 -0300

    Update main.c

--- Sources/main.c
+++ Sources/main.c
@@ -4,11 +4,10 @@
 int main()
 {
     printf("*****************************************************\n");
+    printf("*                                                   *\n");
     printf("*        Ep2 - Compactacao e Descompactacao         *\n");
     printf("*                                                   *\n");
     printf("*                   Criado Por:                     *\n");
-    printf("*         31746357 - Davi Gomes Seagull             *\n");
-    printf("*         31746357 - Davi Gomes Seagull             *\n");
     printf("*         31746357 - Davi Gomes Seagull             *\n");
     printf("*                                                   *\n");
     printf("*****************************************************\n");
