d9e31267536013b70c7ce4927ff29fb3b3118a50
DaviSeagull <davi.seagull@gmail.com>
2020-11-07 01:42:26 -0300

    Alterações no menu

--- Sources/main.c
+++ Sources/main.c
@@ -2,50 +2,71 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void cleanScreen() {
+    system("cls||clear");
+}
+
 void menu() {
 
-  printf("*****************************************************\n");
-  printf("*                                                   *\n");
-  printf("*          Projeto e Analise de Algoritmos          *\n");
-  printf("*        Ep2 - Compactacao e Descompactacao         *\n");
-  printf("*                                                   *\n");
-  printf("*                    Criado Por:                    *\n");
-  printf("*           31746357 - Davi Gomes Seagull           *\n");
-  printf("*                                                   *\n");
-  printf("*****************************************************\n");
+    printf("*****************************************************\n");
+    printf("*                                                   *\n");
+    printf("*          Projeto e Analise de Algoritmos          *\n");
+    printf("*        Ep2 - Compactacao e Descompactacao         *\n");
+    printf("*                                                   *\n");
+    printf("*                    Criado Por:                    *\n");
+    printf("*           31746357 - Davi Gomes Seagull           *\n");
+    printf("*                                                   *\n");
+    printf("*****************************************************\n");
 
-  printf("\nPara sair do programa digite qualquer caracter\n");
-  printf("1 - Exercicio 1\n");
-  printf("2 - Exercicio 2\n");
-  printf("3 - Exercicio 3\n\n");
+    printf("\nMenu de escolha do exercicio \n");
+    printf("0 - Sair\n");
+    printf("1 - Exercicio 1\n");
+    printf("2 - Exercicio 2\n");
+    printf("3 - Exercicio 3\n");
 
-  printf("Opcao: ");
-  int opc;
-  scanf("%d", &opc);
+    printf("\nOpcao: ");
+    int opc;
+    scanf("%d", &opc);
 
-  bool quit = false;
-  while(!quit) {
-    switch (opc) {
-    case 1:
-//      ex1();
-      quit = true;
-    case 2:
-//      ex2();
-      quit = true;
-    case 3:
-//      ex3();
-      quit = true;
-    default:
-      quit = true;
+    while(true) {
+        switch (opc) {
+            case 0:
+                cleanScreen();
+                exit(0);
+                break;
+            case 1:
+                cleanScreen();
+                ex1();
+                break;
+            case 2:
+                cleanScreen();
+                ex2();
+                break;
+            case 3:
+                cleanScreen();
+                ex3();
+                break;
+            default:
+              printf("Voce digitou uma opcao invalida. Tente novamente\n");
+              break;
+        }
     }
-  }
+}
+
+void ex1(){
 
 }
 
-int main()
-{
+void ex2(){
 
-  menu();
-  return 0;
+}
+
+void ex3(){
+
+}
+int main() {
+
+    menu();
+    return 0;
 
 }
