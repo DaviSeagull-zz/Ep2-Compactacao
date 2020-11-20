ca947eb9f1fad2fe48e513da1183547b55c9eea4
DaviSeagull <davi.seagull@gmail.com>
2020-11-20 16:41:04 -0300

    finalizacao do ex2 e criacao das funcoes fillNull e removeHyphen

--- Sources/main.c
+++ Sources/main.c
@@ -18,33 +18,120 @@ void cleanScreen() {
     system("cls||clear");
 }
 
+//funcao para preencher a string de null
+void fillNull(char * s){
 
-void decompression(char *s){ //precisamos arrumar para receber o vetor do Ex1/Ex3
-    char s1[1000];
+    for(int i = 0; i <= strlen(s); i++)
+        s[i] = NULL;
+
+}
+
+//funcao para receber a string desejada pelo usuario
+char * setString() {
+
+    char * s; //ponteiro para char
+    char aux[100000]; //vetor para receber a string do usuario
+    int string_counter = 0; //variavel para contar o tamanho da string
+
+    printf("String desejada: ");
+    fflush(stdin);
+    gets(aux); //recebe a String desejada
+
+    //for para contar quantos caracteres a String do usuario possui
+    for(int i = 0; i < 100000; i++){
+        if(aux[i] == "\0")
+            break;
+        string_counter++;
+    }
+    char aux2[string_counter]; //cria vetor com tamanho = a quantidade de caracteres na String
+
+    //for para salvar todos os valores no vetor com tamanho correto
+    for(int i = 0; i < string_counter; i++){
+        aux2[i] = aux[i];
+    }
+
+    //salva os valores de aux2 no ponteiro s
+    s = &aux2;
+
+    return s;
+
+}
+
+//funcao para deixar todas as letras da string em minusculo
+void setLower(char * s) {
+
+    char a[strlen(s)]; //vetor de char do tamanho da string
+
+    strcpy(a,s); //copia dos valores da string para o vetor a
+
+    // for para salvar em lowercase cada posicao do vetor
+    for (int i = 0; i < (strlen(s)); i++)
+        s[i] = tolower(a[i]);
+
+}
+
+//funcao para remover o hifen da string recebida
+void removeHyphen (char * s) {
+
+    int size_s = strlen(s); //variavel com o tamanho da string
+
+    char aux[size_s]; //vetor aux do tamanho da string
+    char c[2] = "-"; //variavel com o hífen
+    char * token; //ponteiro utilizado para salvar o token
+
+    strcpy(aux, s); //copia do s para aux
+    token = strtok(aux, c); //primeiro token
+
+    fillNull(s);
+
+    while( token != NULL ) {
+        strcat(s,token);
+        token = strtok(NULL, c);
+    }
+
+}
+
+//funcao para descomprimir string
+void decompression(char *s){
+
+    char aux1[1000]; //declaracao do vetor auxiliar 1
+    char aux2[1000]; //declaracao do vetor auxiliar 2
     int i = 0, j = 0, k, n;
 
-    while(s[i])
-    {
-        s1[j] = s[i];
-        i++;
-        j++;
+    fillNull(aux2); //preencher o aux2 de null
+
+    while(s[i]) {
         n = 0;
 
-        while(s[i] >= '1' && s[i] <= '9') {
+        //enquanto nao for numero ele salva no aux1
+        while(!isdigit(s[i])){
+            aux1[j] = s[i];
+            i++;
+            j++;
+        }
+
+        //calcula quantas vezes terá que repetir o que esta salvo em aux1
+        while(isdigit(s[i])) {
             n *= 10;
             n += s[i] - '0';
             i++;
         }
-        for(k = 0; k < n - 1; k++)
-            s1[j+k] = s1[j-1];
-         if(n > 0)
+
+        //for para repetir a string em aux1 n vezes
+        for(k = 0; k < n; k++)
+            strcat(aux2, aux1);
+
+        if(n > 0)
             j+=n-1;
+
+        fillNull(aux1); //preencher o aux1 de null
+
+        //limpar o contador de aux1
+        j = 0;
     }
 
-    s1[j]='\0';
-
     printf("\nString Compactada: %s", s);
-	printf("\nString Descompactada: %s",s1);
+	printf("\nString Descompactada: %s", aux2);
 
     return;
  }
@@ -90,99 +177,50 @@ char * simpleCompression(char *s){
 
 }
 
-//funcao para receber a string desejada pelo usuario
-char * setString() {
-
-    char * s; //ponteiro para char
-    char aux[100000]; //vetor para receber a string do usuario
-    int string_counter = 0; //variavel para contar o tamanho da string
-
-    printf("String desejada: ");
-    fflush(stdin);
-    gets(aux); //recebe a String desejada
-
-    //for para contar quantos caracteres a String do usuario possui
-    for(int i = 0; i < 100000; i++){
-        if(aux[i] == "\0")
-            break;
-        string_counter++;
-    }
-    char aux2[string_counter]; //cria vetor com tamanho = a quantidade de caracteres na String
-
-    //for para salvar todos os valores no vetor com tamanho correto
-    for(int i = 0; i < string_counter; i++){
-        aux2[i] = aux[i];
-    }
-    s = &aux2;
-    return s;
-
-}
-
-//funcao para deixar todas as letras da string em minusculo
-void setLower(char * s) {
-
-    char a[strlen(s)]; //vetor de char do tamanho da string
-    strcpy(a,s); //copia dos valores da string para o vetor a
-    for (int i = 0; i < (strlen(s)); i++)
-        s[i] = tolower(a[i]);
-
-}
-
-//funcao que pergunta se quer descompactar a string compactada
-void subMenuCompression(char *s) {
-
-    int opc_menu;
-
-    do {
-        printf("\n\nDeseja descompactar a string compactada?");
-        printf("\n0 - Nao, voltar ao menu");
-        printf("\n1 - Sim");
-        printf("\n\nOpcao: ");
-        scanf("%i", &opc_menu);
-
-        switch(opc_menu){
-        case 0:
-            cleanScreen();
-            break;
-        case 1:
-            cleanScreen();
-            decompression(s);
-            break;
-        default:
-            printf("Voce digitou uma opcao invalida. Tente novamente");
-        }
-    } while(opc_menu != 0);
-
-}
-
 //funcao para o ex1
 void ex1(){
 
-    char * s; //ponteiro para char
-    s = setString(); //chamada da funcao setString
-    setLower(s); //chamada da funcao setLower
-    int t = strlen(s); //variavel para armazenar o tamanho da String
+    char * s_ex1; //ponteiro para string recebida
+    s_ex1 = setString(); //chamada da funcao setString
+
+    setLower(s_ex1); //chamada da funcao setLower
+
+    int t = strlen(s_ex1); //variavel para armazenar o tamanho da String
+
     printf("\nTamanho da string: %i\n", t);
     printf("String: ");
-    puts(s);
+    puts(s_ex1);
     printf("String compactada: ");
-    simpleCompression(s); //chamada da funcao simpleCompression
-    subMenuCompression(s);
 
+    simpleCompression(s_ex1); //chamada da funcao simpleCompression
     return;
 
 }
 
 //funcao para o ex2
 void ex2(){
-    char * s_ex2;
-    s_ex2 = setString();
-    decompression(s_ex2);
+
+    char * s_ex2; //ponteiro para string recebida
+    s_ex2 = setString(); //chamada da funcao setString
+
+    setLower(s_ex2); //chamada da funcao setLower
+    removeHyphen(s_ex2); //chamada da funcao removeHyphen
+    decompression(s_ex2); //chamada da funcao decompression
+
 }
 
 //funcao para o ex3
 void ex3(){
+    char * s_ex3; //ponteiro para char
+    s_ex3 = setString(); //chamada da funcao setString
 
+    setLower(s_ex3); //chamada da funcao setLower
+    int t = strlen(s_ex3); //variavel para armazenar o tamanho da String
+
+    printf("\nTamanho da string: %i\n", t);
+    printf("String: ");
+    puts(s_ex3);
+    printf("String compactada: ");
 }
 
 //funcao do Menu
@@ -246,3 +284,4 @@ int main() {
     return 0;
 
 }
+
